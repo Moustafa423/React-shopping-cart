0ec86e2c47a7319ee92dbac83e4ac87e02cc2ac2
Moustafa423 <mustafa.elrawy.2020@gmail.com>
2022-03-10 18:57:50 +0200

    products-comp

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,17 +1,23 @@
-import React from 'react'
-import Header from './component/Header/Header';
-import Footer from './component/Footer/Footer';
+import React,{useState} from "react";
+import Header from "./component/Header/Header";
+import Footer from "./component/Footer/Footer";
+import data from "./data.json"
+import Products from "./component/Products/Products";
 function App() {
+  const [products,setProducts] = useState(data);
+  console.log(products)
   return (
-    <div className='layout'>
-        <Header />
-        <main>
-          Contain
-        </main>
-        <Footer />
-
-      </div>
-  )
+    <div className="layout">
+      <Header />
+      <main>
+        <div className="wrapper">
+          <Products products={products}/>
+          <div className="filter"> Filter </div>
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
