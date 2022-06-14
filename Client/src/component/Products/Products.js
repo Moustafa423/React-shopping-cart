e2b4626619545493e4321de0017ca6999d0909b5
Moustafa423 <mustafa.elrawy.2020@gmail.com>
2022-06-14 14:16:15 +0200

    Filter-comp

--- Client/src/component/Products/Products.js
+++ Client/src/component/Products/Products.js
@@ -1,20 +1,34 @@
-import React from "react";
-import "../../css/Products/Products.css"
+import React, { useState } from "react";
+import "../../css/Products/Products.css";
+import Modal from "react-modal";
 function Products(props) {
-  console.log(props);
+  const [product, setProduct] = useState("");
+
+  const openModal = (product) => {
+    setProduct(product);
+  };
   return (
     <div className="products">
-      {" "}
       {props.products.map((product) => (
-          <div className="product-item" key={product.id}>
+        <div className="product-item" key={product.id}>
+          <a href="#" onClick={() => openModal(product)}>
             <img src={product.imageUrl} alt={product.title} />
-            <div className="product-desc">
-              <p>{product.title}</p>
-              <span>{product.price}</span>
-            </div>
-            <button>Add To Cart</button>
+          </a>
+          <div className="product-desc">
+            <p>{product.title}</p>
+            <span>{product.price}</span>
           </div>
-      ))}{" "}
+          <button>Add To Cart</button>
+        </div>
+      ))}
+
+      <Modal isOpen={product}>
+        <div className="prroduct-info">
+          <img src={product.imageUrl} alt={product.title} />
+          <p>{product.title}</p>
+          <p>{product.desc}</p>
+        </div>
+      </Modal>
     </div>
   );
 }
